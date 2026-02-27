95feb8a56c347cf19aff938daa1482c84e580081
Hossein-Falah <hosseinfalah2021@gmail.com>
2026-02-27 14:54:04 +0330

    fix: update session cookie settings to enable secure flag for production environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -42,7 +42,7 @@ app.use(
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: "none", 
-    secure: false,             
+    secure: true,             
     httpOnly: true,
     domain: "task-api.teleservat.com",
     path: '/'
